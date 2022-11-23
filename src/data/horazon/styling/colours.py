2c32d82d5f12a61bc3ac8eba1cdeb94d2005b2a4
oaao <oliver.antoniu@gmail.com>
2022-11-22 17:44:22 -0700

    [feat] data — colours (semi-WIP, d2 vs. d2r partially untested)

---
+++ src/data/horazon/styling/colours.py
@@ -0,0 +1,25 @@
+"""Colour tags used by D2R text formatting."""
+
+WHITE     = 'ÿc0'
+RED       = 'ÿc1'  
+LIME      = 'ÿc2'  # set
+BLUE      = 'ÿc3'  # magic
+GOLD      = 'ÿc4'  # unique
+GREY      = 'ÿc5'  # ethereal / socketed
+BLACK     = 'ÿc6'
+GOLD2     = 'ÿc7'  # TODO: tan? different than gold@4? figure this out
+ORANGE    = 'ÿc8'  # crafted
+YELLOW    = 'ÿc9'  # rare
+
+# TODO: these need to be systematically retested; some no longer work in d2r
+WHITE2    = 'ÿc='
+BROWN     = 'ÿc!'
+DRK_BLUE  = 'ÿc"'
+LT_BLUE   = 'ÿc+'
+DRK_GREEN = 'ÿc:'  # works for sure
+LT_GREEN  = 'ÿc<'
+PURPLE    = 'ÿc;'
+PINK      = 'ÿcO'
+LILAC     = 'ÿcP'
+
+UNKNOWN1  = 'ÿc-'
