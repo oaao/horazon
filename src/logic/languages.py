64fe2d1ec3ede4026f580a5bd678b2719771704d
oaao <oliver.antoniu@gmail.com>
2022-11-24 21:36:22 -0700

    [feat] logic — stage languages logic

---
+++ src/logic/languages.py
@@ -0,0 +1,19 @@
+"""Language processing."""
+
+LANGUAGES_AVAILABLE = ("enUS",)
+
+LANGUAGE_MAP = {
+    "deDE": "Deutsch",
+    "enUS": "English (US)",
+    "esES": "Español (España)",
+    "esMX": "Español (México)",
+    "frFR": "Français",
+    "itIT": "Italiano",
+    "jaJP": "日本語",
+    "koKR": "한국어",
+    "plPL": "Polski",
+    "ptBR": "Português (Brasil)",
+    "ruRU": "Русский",
+    "zhCN": "中文 (Simplified)",
+    "zhTW": "中文 (Taiwan)",
+}
